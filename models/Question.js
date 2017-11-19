811cbab5574b99f16ae80ce067f2688c98fdc9bd
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-19 17:19:39 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -24,7 +24,7 @@ class Question{
     return new Promise(function(resolve){
       db.run(sql, [self.content], function(err, result){
         self.id = this.id
-        resolve("Row inserted!")
+        resolve(self)
       })
     })
   }
