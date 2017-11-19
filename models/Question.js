6fc47b7e753e9f975588126083969f98fb030e84
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-19 16:44:32 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -20,47 +20,13 @@ class Question{
 
   insert() {
     const self = this
-    const SQL = `INSERT INTO questions (content) VALUES (?)`
     return new Promise(function(resolve){
-      
-    resolve(self)
+      db.run(sql, [this.content], function(err, result){
+        resolve("Row inserted!")
+      })
     })
-
   }
-
-
 }
-
-
-
-
-
-
-//   insert(){
-//     const self = this
-//     const sql = `INSERT INTO questions (name, age) VALUES (?, ?)`
-//     console.log(`Inserting user ${self.name} into database...`)
-//     return new Promise(function(resolve){
-//       db.run(sql, [self.name, self.age], function(){
-//         console.log(`...user ${this.lastID} inserted into database`)
-//         self.id = this.lastID
-//         resolve(self)
-//       })
-//     })
-//       //  `INSERT INTO questions (content) VALUES (?)`
-//       // self.id = this.lastID
-//       // resolve(self)
-//   })
-// }
-// //   }
-// // }
-//
-// // db.run(sql, [self.name, self.age], function(){
-// //   console.log(`...user ${this.lastID} inserted into database`)
-// //   self.id = this.lastID
-// //   resolve(self)
-// // })
-
 
 
 module.exports = Question;
