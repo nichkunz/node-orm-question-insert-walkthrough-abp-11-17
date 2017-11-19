00d743cdd182316487bc7da25ed82d97bf31dd22
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-19 16:43:44 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -7,10 +7,10 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
         resolve("questions table created")
-      })      
+      })
     })
   }
 
@@ -18,6 +18,49 @@ class Question{
     this.content = content
   }
 
+  insert() {
+    const self = this
+    const SQL = `INSERT INTO questions (content) VALUES (?)`
+    return new Promise(function(resolve){
+      
+    resolve(self)
+    })
+
+  }
+
+
 }
+
+
+
+
+
+
+//   insert(){
+//     const self = this
+//     const sql = `INSERT INTO questions (name, age) VALUES (?, ?)`
+//     console.log(`Inserting user ${self.name} into database...`)
+//     return new Promise(function(resolve){
+//       db.run(sql, [self.name, self.age], function(){
+//         console.log(`...user ${this.lastID} inserted into database`)
+//         self.id = this.lastID
+//         resolve(self)
+//       })
+//     })
+//       //  `INSERT INTO questions (content) VALUES (?)`
+//       // self.id = this.lastID
+//       // resolve(self)
+//   })
+// }
+// //   }
+// // }
+//
+// // db.run(sql, [self.name, self.age], function(){
+// //   console.log(`...user ${this.lastID} inserted into database`)
+// //   self.id = this.lastID
+// //   resolve(self)
+// // })
+
+
 
 module.exports = Question;
